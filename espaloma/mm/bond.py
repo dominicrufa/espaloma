4ce737906195a6115488f3a9bd2fa0ccc05c93e0
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-02-15 10:41:40 -0500

    Update bond.py
    modify `harmonic bond` to accommodate cosine angles

--- espaloma/mm/bond.py
+++ espaloma/mm/bond.py
@@ -7,7 +7,7 @@ import espaloma as esp
 # =============================================================================
 # MODULE FUNCTIONS
 # =============================================================================
-def harmonic_bond(x, k, eq):
+def harmonic_bond(x, k, eq, use_cos: bool=False):
     """Harmonic bond energy.
 
     Parameters
@@ -30,7 +30,7 @@ def harmonic_bond(x, k, eq):
 
     # NOTE:
     # 0.25 because all bonds are calculated twice
-    return 0.5 * esp.mm.functional.harmonic(x=x, k=k, eq=eq)
+    return 0.5 * esp.mm.functional.harmonic(x=x, k=k, eq=eq, use_cos=use_cos)
 
 
 def gaussian_bond(x, coefficients):
